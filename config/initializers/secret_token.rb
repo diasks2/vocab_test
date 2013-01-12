01806312e6ea9d8e67571483eed8ee59af8220af
Kevin <diasks2@gmail.com>
2013-01-12 15:47:09 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocabTest::Application.config.secret_token = 'd9de80724894654b40dd1944cc949feef223957066dc39ec62cca962f400a672036fa195997c0b7f3c172c7955fcd258265bbf6c70b3b65855978aab6859dbf1'

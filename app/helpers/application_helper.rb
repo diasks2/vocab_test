e2c28c2edf46e1ea90dfdba0057df1e307e9c653
Kevin <diasks2@gmail.com>
2013-01-13 08:51:27 +0900

    Work on mobile and tablet template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+  	def full_title(page_title)
+    	base_title = "Vocabulary Assessment Test"
+    	if page_title.empty?
+      		base_title
+    	else
+      		"#{base_title} | #{page_title}"
+    	end
+  	end
+
+  	def description(page_description)
+    	base_description = "Vocabulary Assessment Test"
+    	if page_description.empty?
+      		base_description
+    	else
+      		"#{page_description}"
+    	end
+  	end
+
+  	def keywords(page_keywords)
+    	base_keywords = "Vocabulary Assessment Test"
+    	if page_keywords.empty?
+      		base_keywords
+    	else
+      		"#{page_keywords}"
+    	end
+  	end
+
 end
